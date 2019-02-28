9fc5ecc2787cebd7956fd70d924e5b646c9fe90b
gengjianchun <geng.jianchun@163.com>
2019-02-28 16:10:11 +0800

    Update SpiderStart.java

--- ZiRoomSpider/src/main/java/com/geng/ZiRoomSpider/SpiderStart.java
+++ ZiRoomSpider/src/main/java/com/geng/ZiRoomSpider/SpiderStart.java
@@ -6,10 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.geng.ZiRoomSpider.service.SpiderService;
 
 public class SpiderStart {
-//测试代码冲突
-	//啊啊啊啊啊
-	//aaaaa
-	///qqqqqq
+
 	public static void main(String[] args) throws Exception {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
