8e831f022d427fba76181e46e3527b0d48153f80
gengjianchun <gengjianchun@ele-cloud.com>
2019-02-28 15:55:35 +0800

    111111

--- ZiRoomSpider/src/main/java/com/geng/ZiRoomSpider/SpiderStart.java
+++ ZiRoomSpider/src/main/java/com/geng/ZiRoomSpider/SpiderStart.java
@@ -8,6 +8,8 @@ import com.geng.ZiRoomSpider.service.SpiderService;
 public class SpiderStart {
 //测试代码冲突
 	//啊啊啊啊啊
+	//aaaaa
+	///qqqqqq
 	public static void main(String[] args) throws Exception {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
